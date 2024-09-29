cc61e221df2b1b81c3c9fc2028db4c3fd9d69f48
Bhuvanesh Selvaraj <bhuvanesh.sel@gmail.com>
2024-09-28 17:32:05 -0700

    Censoring

--- Censoring.java
+++ Censoring.java
@@ -3,20 +3,26 @@ import java.util.StringTokenizer;
 
 public class Censoring {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter pw = new PrintWriter(System.out);
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //PrintWriter pw = new PrintWriter(System.out);
+        BufferedReader br = new BufferedReader(new FileReader("censor.in"));
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("censor.out")));
 
         String s = br.readLine();
         String t = br.readLine();
 
-        int i = 0;
-        int j = 0;
-        int k = 0;
-        while (i < s.length()) {
-            while (t.charAt(j) == s.charAt(i)) {
-                i++;
-                j++;
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            result.append(s.charAt(i));
+            if (result.length() >=t.length()) {
+                String check = result.substring(result.length()-t.length(), result.length());
+                if (check.equals(t)) {
+                    result.delete(result.length()-t.length(), result.length());
+                }
             }
         }
+        pw.println(result);
+        pw.close();
+
     }
 }
